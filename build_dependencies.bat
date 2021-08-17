c3c12d8468be7dfe9a463aeb1bda4f91f02de266
David Goosen <deadVertex@gmail.com>
2021-08-17 21:10:09 +1200

    Batch file for building dependencies on windows with CMake profiles

--- build_dependencies.bat
+++ build_dependencies.bat
@@ -3,8 +3,19 @@
 if not exist build mkdir build
 if not exist build\dependencies mkdir build\dependencies
 
+REM CMake bug? configurePreset field in buildPreset is being ignored
 REM Build glfw
 COPY dependencies\glfw.json dependencies\glfw\CMakePresets.json
-cmake -S .\dependencies\glfw
-cmake --build build\dependencies\glfw
-cmake --install build\dependencies\glfw
+pushd dependencies\glfw
+cmake --preset=default .
+cmake --build build --config=Release 
+cmake --install build
+popd
+
+REM Build assimp
+COPY dependencies\assimp.json dependencies\assimp\CMakePresets.json
+pushd dependencies\assimp
+cmake --preset=default .
+cmake --build build --config=Release 
+cmake --install build
+popd
