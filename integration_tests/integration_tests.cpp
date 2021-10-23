9d6446f11ec7ccb0a176ee931368e98b557e913e
David Goosen <deadVertex@gmail.com>
2021-10-24 10:40:51 +1300

    Remove Vulkan integration test

--- integration_tests/integration_tests.cpp
+++ integration_tests/integration_tests.cpp
@@ -7,55 +7,11 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-#ifdef PLATFORM_WINDOWS
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <windows.h>
-#elif defined(PLATFORM_LINUX)
-#endif
-
-// Move to vulkan specific module?
-#include <vulkan/vulkan.h>
-#ifdef PLATFORM_WINDOWS
-#include <vulkan/vulkan_win32.h>
-#endif
-
-#include <GLFW/glfw3.h>
-#include <GLFW/glfw3native.h>
-
-#include "platform.h"
-
-// FIXME: Copied from main.cpp
-struct DebugReadFileResult
-{
-    void *contents;
-    u32 length;
-};
-
-#define DebugReadEntireFile(NAME) DebugReadFileResult NAME(const char *path)
-typedef DebugReadEntireFile(DebugReadEntireFileFunction);
-#define DebugFreeFileMemory(NAME) void NAME(void *memory)
-typedef DebugFreeFileMemory(DebugFreeFileMemoryFunction);
-
-internal void* AllocateMemory(u64 size, u64 baseAddress = 0);
-internal void FreeMemory(void *p);
-internal DebugReadEntireFile(ReadEntireFile);
-#include "vulkan_renderer.h"
-
 #include "cpu_ray_tracer.h"
-
-#include "vulkan_renderer.cpp"
 
 #include "mesh.cpp"
 
 #include "cmdline.cpp"
-
-
-DebugReadEntireFile(ReadEntireFile)
-{
-    DebugReadFileResult result = {};
-    return result;
-}
 
 global const char *g_AssetDir = "../assets";
 
@@ -67,12 +23,6 @@ void setUp(void)
 void tearDown(void)
 {
     // clean stuff up here
-}
-
-// FIXME: Copied from main.cpp
-internal void GlfwErrorCallback(int error, const char *description)
-{
-    LogMessage("GLFW Error (%d): %s.", error, description);
 }
 
 // Integration tests
@@ -88,27 +38,6 @@ void TestLoadMesh()
     TEST_ASSERT_GREATER_THAN_UINT(0, meshData.vertexCount);
 
     free(memory);
-}
-
-void TestUploadCubemapToGPU()
-{
-    // FIXME: Copied from main.cpp
-    glfwSetErrorCallback(GlfwErrorCallback);
-    glfwInit();
-
-    u32 windowWidth = 256;
-    u32 windowHeight = 256;
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow *window = glfwCreateWindow(windowWidth,
-        windowHeight, "integration tests", NULL, NULL);
-    TEST_ASSERT_TRUE(window != NULL);
-
-    // Create Vulkan Renderer
-    VulkanRenderer renderer = {};
-    VulkanInit(&renderer, window);
-
-    glfwTerminate();
 }
 
 // FIXME: Copied from main.cpp
@@ -129,6 +58,5 @@ int main(int argc, char **argv)
     LogMessage("Asset directory set to \"%s\".", g_AssetDir);
 
     RUN_TEST(TestLoadMesh);
-    RUN_TEST(TestUploadCubemapToGPU);
     return UNITY_END();
 }
